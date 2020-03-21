f2883c944d97da5c5f6cd81a74a548f08dce5afa
Mark Alexieff <mark.alexieff@outlook.com>
2020-03-21 13:21:54 -0700

    Initial Work - getting set up.

---
+++ CH9 X2/CH9 X2.h
@@ -0,0 +1,36 @@
+#pragma once
+
+/*
+Design and implement a Name_pairs class holding (name,age) pairs where name is a string and age
+is a double. Represent that as a vector<string> (called name) and a vector<double> (called age) 
+member. Provide an input operation read_names() that reads a series of names. Provide a 
+read_ages() operation that prompts the user for an age for each name. Provide a print() 
+operation that prints out the (name[i],age[i]) pairs (one per line) in the order determined 
+by the name vector. Provide a sort() operation that sorts the name vector in alphabetical 
+order and reorganizes the age vector to match. Implement all “operations” as member functions. 
+Test the class (of course: test early and often).
+
+This is the header file with the Class definition
+*/
+
+namespace CH9X2_Date {
+
+	class Date
+	{
+	public:
+		Date();
+		~Date();
+
+	private:
+
+	};
+
+	Date::Date()
+	{
+	}
+
+	Date::~Date()
+	{
+	}
+
+}
--- CH9 X2/Source.cpp
+++ CH9 X2/Source.cpp
@@ -1,4 +1,13 @@
 /*  Program Description
+Design and implement a Name_pairs class holding (name,age) pairs where name is a string
+and age is a double. Represent that as a vector<string> (called name) and a vector<double> 
+(called age) member. Provide an input operation read_names() that reads a series of names. 
+Provide a read_ages() operation that prompts the user for an age for each name. 
+Provide a print() operation that prints out the (name[i],age[i]) pairs (one per line) 
+in the order determined by the name vector. Provide a sort() operation that sorts the 
+name vector in alphabetical order and reorganizes the age vector to match. Implement all
+“operations” as member functions. Test the class (of course: test early and often).
+
 */
 
 /*
@@ -6,6 +15,7 @@ Standard opening
 */
 
 #include "C:\Users\mark.alexieff\source\repos\std_lib_facilities.h"
+#include "CH9 X2.h"
 
 /*
 global variables
